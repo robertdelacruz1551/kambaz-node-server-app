f13fde4fde33a161ea1e1774ead3d2cd43298d23
Robert De La Cruz <snailinslowmotion@gmail.com>
2025-08-03 11:30:25 -0400

    A5

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors()); 
+app.use(express.json());
 
 Lab5(app);
 Hello(app);
 
-app.listen(4000);
+app.listen(process.env.PORT || 4000)
