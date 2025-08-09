ed983f9ba84ef6bc1aebe368c53496967f9bc363
Robert De La Cruz <snailinslowmotion@gmail.com>
2025-08-09 11:29:31 -0400

    A6

---
+++ Kambaz/Users/dao.js
@@ -0,0 +1,10 @@
+import model from "./model.js";
+import { v4 as uuidv4 } from "uuid";
+
+export const createUser = (user) => {} // implemented later
+export const findAllUsers = () => model.find();
+export const findUserById = (userId) => model.findById(userId);
+export const findUserByUsername = (username) =>  model.findOne({ username: username });
+export const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
+export const updateUser = (userId, user) =>  model.updateOne({ _id: userId }, { $set: user });
+export const deleteUser = (userId) => model.deleteOne({ _id: userId });
